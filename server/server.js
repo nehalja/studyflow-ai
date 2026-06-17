dd90a7acc57a0e6b2e78796fe9fc49aa3a8aeecf
Nehal <nehaljain1412007@gmail.com>
2026-06-17 16:34:04 +0530

    Implement user authentication with JWT

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -15,4 +19,8 @@ mongoose
     console.error("MongoDB Error:");
     console.error(err);
   });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
 
--- server/test.js
+++
@@ -1,9 +0,0 @@
-const dns = require("dns");
-
-dns.resolveSrv(
-  "_mongodb._tcp.cluster0.vctaxk2.mongodb.net",
-  (err, records) => {
-    console.log("Error:", err);
-    console.log("Records:", records);
-  }
-);
