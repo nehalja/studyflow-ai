04d8bad5fd25c6c8d1600bdcdf40fe1cfc5788c3
Nehal <nehaljain1412007@gmail.com>
2026-06-23 03:49:55 +0530

    Create subject model

---
+++ server/models/Subject.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const subjectSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Subject", subjectSchema);
